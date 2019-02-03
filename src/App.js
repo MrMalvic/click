c6da72df121c66873912ec357195b5d23402d776
Malumbo Mutale <mutalevictor6@gmail.com>
2019-02-03 16:21:58 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
